57368147de7d18b1b1a7aaee17a595eb89d16d39
Thomas CHEZIERES <tchezier@e2r13p11.42.fr>
2014-11-21 17:29:10 +0100

    2d

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -6,26 +6,32 @@
 /*   By: tchezier <tchezier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/19 11:00:27 by tchezier          #+#    #+#             */
-/*   Updated: 2014/11/20 10:30:58 by tchezier         ###   ########.fr       */
+/*   Updated: 2014/11/21 17:25:26 by tchezier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "get_next_line.h"
 
-int get_next_line(int const fd, char ** line)
+int get_next_line(int const fd, char **line)
 {
 	char buf[BUF_SIZE + 1];
 	int ret;
+	int x;
 
-	while ((ret = read(fd, buf, BUF_SIZE)) != 0)
+	x = 0;
+	while ((ret = read(fd, buf, BUF_SIZE)))
 	{
-		buf[ret] = '\0';
-		ft_putstr(buf);
-		//*line = strdup(buf);
-		//line++;
+			buf[ret] = '\0';
+			ft_putnbr(ret);
+			ft_putchar('\n');
+			ft_putstr(buf);
+			ft_strcpy(line[x], buf);
+			x++;
 	}
-	*line = strdup(buf);
+	ft_putstr("\n on sort du while !");
+	ft_putchar('\n');
+	ft_putstr("copie reussi");
 	return (0);
 }
 
@@ -33,20 +39,20 @@ int main()
 {
 	int fd;
 	int back;
-	char **line;
+	char **line2;
 
-	line = NULL;
+	line2 = NULL;
 	fd = open("getlines", O_RDONLY);
 	ft_putnbr(fd);
+	ft_putchar('\n');
 	if (fd == -1)
 	{
 		ft_putstr("open() error");
 		return (1);
 	}
-	while (back != 0)
-	{
-		back = get_next_line(fd, line);
-	}
+	back = get_next_line(fd, line2);
+	ft_putnbr(back);
+	ft_putchar('\n');
 	if (close(fd) == -1)
 	{
 		ft_putstr("close() error");
